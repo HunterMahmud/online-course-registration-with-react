109819b8e17f9f534cef1783bc6c33b2ef78c06a
huntermahmud <mahmudthedatahunter@gmail.com>
2024-03-13 12:43:21 +0600

    initial state

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Courses from './components/courses/Courses'
+
+function App() {
+
+  return (
+    <div>
+      
+      <h1>hello</h1>
+      <Courses/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/courses/Courses.jsx
@@ -0,0 +1,11 @@
+import Course from "./Course";
+
+function Courses(props) {
+    return (
+        <div>
+            <Course/>
+        </div>
+    );
+}
+
+export default Courses;
